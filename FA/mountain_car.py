222d4d012fb9c1cb8ea8812698cbe71abba11c70
SamKirkiles <sam.kirkiles@gmail.com>
2018-06-11 15:10:23 -0400

    updated linear function approx

--- FA/mountain_car.py
+++ FA/mountain_car.py
@@ -6,17 +6,28 @@ env = gym.make('MountainCar-v0')
 
 state = env.reset()
 
+num_episodes = 3
+
+#Parameter vector
+w = np.random.rand(3)
+
 def Q(state,action,w):
-	pass
+	x = np.append(state,action)
+	print(x.dot(w))
 
-for i in range(1000):
 
-	env.render()
-	action = env.action_space.sample()
-	next_state, reward, done, _ = env.step(action)
-	print(state)
-	print(action)
-	state = next_state
+
+for e in range(num_episodes):
+	for i in range(1000):
+
+		env.render()
+		action = env.action_space.sample()
+		next_state, reward, done, _ = env.step(action)
+		value = Q(state,action,w)
+		if done:
+			print("finished episode")
+			break
+		state = next_state
 
 
 env.close()
