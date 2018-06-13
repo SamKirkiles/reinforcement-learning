cfa5b704d3faa9303530b021419672b06eeafecc
Sam Kirkiles <sam.kirkiles@gmail.com>
2018-06-13 14:08:13 -0400

    solved mountain car

--- FA/mountain_car.py
+++ FA/mountain_car.py
@@ -1,9 +1,12 @@
-
+import matplotlib
 import numpy as np
 import gym
 import matplotlib.pyplot as plt
 from sklearn.kernel_approximation import RBFSampler
 from sklearn.linear_model import SGDClassifier
+from mpl_toolkits.mplot3d import Axes3D
+import sklearn.pipeline
+import sklearn.preprocessing
 
 env = gym.make('MountainCar-v0')
 env._max_episode_steps = 300
@@ -14,7 +17,7 @@ discount_factor = 1.0
 alpha = 0.01
 
 #Parameter vector
-w = np.zeros((3,200))
+w = np.zeros((3,400))
 
 # Plots
 plt_actions = np.zeros(3)
@@ -23,18 +26,25 @@ episode_rewards = np.zeros(num_episodes)
 # Get satistics over observation space samples for normalization
 observation_examples = np.array([env.observation_space.sample() for x in range(10000)])
 scale_mean = np.mean(observation_examples,axis=0)
-scale_max = np.max(observation_examples,axis=0)
-scale_min = np.min(observation_examples,axis=0)
+scale_std = np.std(observation_examples,axis=0)
+scaler = sklearn.preprocessing.StandardScaler()
+scaler.fit(observation_examples)
+
 
 # Create radial basis function sampler to convert states to features for nonlinear function approx
-rbf_feature = RBFSampler(gamma=1,n_components=200, random_state=1)
+featurizer = sklearn.pipeline.FeatureUnion([
+        ("rbf1", RBFSampler(gamma=5.0, n_components=100)),
+        ("rbf2", RBFSampler(gamma=2.0, n_components=100)),
+        ("rbf3", RBFSampler(gamma=1.0, n_components=100)),
+        ("rbf4", RBFSampler(gamma=0.5, n_components=100))
+		])
+featurizer.fit(scaler.transform(observation_examples))
 
 # Normalize and turn into feature
 def featurize_state(state):
-	X = (state - scale_mean)/(scale_max - scale_min)
-	X = np.array([X])
-	X_features = rbf_feature.fit_transform(X)
-	return X_features
+	scaled = scaler.transform([state])
+	featurized = featurizer.transform(scaled)
+	return featurized
 
 def Q(state,action,w):
 	value = state.dot(w[action])
@@ -69,6 +79,8 @@ def check_gradients(index,state,next_state,next_action,weight,reward):
 	return grad[0]
 
 
+cost = []
+
 # Our main training loop
 for e in range(num_episodes):
 
@@ -77,7 +89,7 @@ for e in range(num_episodes):
 
 	while True:
 
-		#env.render()
+		env.render()
 		# Sample from our policy
 		action = policy(state,w)
 		# Staistic for graphing
@@ -97,19 +109,37 @@ for e in range(num_episodes):
 		td_error = target - Q(state,action,w)
 
 		# Find gradient with code to check it commented below (check passes)
-		dw = (state).T.dot(td_error)
+		#dw = (state).T.dot(td_error)
+		dw = (td_error).dot(state)
 		
 		#for i in range(4):
 		#	print("First few gradients")
 		#	print(str(i) + ": " + str(check_gradients(i,state,next_state,next_action,w,reward)) + " " + str(dw[i]))
 
 		# Update weight
-		w[action] -= alpha * dw
+		w[action] += alpha * dw
 
 		if done:
 			break
 		# update our state
 		state = next_state
+
+def plot_cost_to_go_mountain_car(num_tiles=20):
+    x = np.linspace(env.observation_space.low[0], env.observation_space.high[0], num=num_tiles)
+    y = np.linspace(env.observation_space.low[1], env.observation_space.high[1], num=num_tiles)
+    X, Y = np.meshgrid(x, y)
+    Z = np.apply_along_axis(lambda _: -np.max([Q(featurize_state(_),a,w) for a in range(3)]), 2, np.dstack([X, Y]))
+
+    fig = plt.figure(figsize=(10, 5))
+    ax = fig.add_subplot(111, projection='3d')
+    surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1,
+                           cmap=matplotlib.cm.coolwarm, vmin=-1.0, vmax=1.0)
+    ax.set_xlabel('Position')
+    ax.set_ylabel('Velocity')
+    ax.set_zlabel('Value')
+    ax.set_title("Mountain \"Cost To Go\" Function")
+    fig.colorbar(surf)
+    plt.show()
 
 
 # Show bar graph of actions chosen
@@ -119,6 +149,7 @@ plt.figure()
 # Plot the reward over all episodes
 plt.plot(np.arange(num_episodes),episode_rewards)
 plt.show()
+plot_cost_to_go_mountain_car()
 
 
 env.close()
